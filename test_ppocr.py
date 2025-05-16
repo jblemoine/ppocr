8d42881f3d0c2c60d2a2532f97025373016d35d5
jblemoine <jeanbaptistelemoine@hotmail.fr>
2025-05-16 16:31:07 +0200

    fix tests

--- test_ppocr.py
+++ test_ppocr.py
@@ -1,20 +1,14 @@
+from pathlib import Path
+
 import cv2
-import numpy as np
-import requests
 
 from ppocr.ppocr import PPOCR
 from ppocr.types import OCRResult
 
 
 def test_ppocr():
-    url = (
-        "https://raw.githubusercontent.com/JaidedAI/EasyOCR/master/examples/english.png"
-    )
-    response = requests.get(url)
-    response.raise_for_status()
-    image_array = np.frombuffer(response.content, np.uint8)
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img_path = Path(__file__).parent / "data" / "english.png"
+    image = cv2.imread(str(img_path))
     ocr = PPOCR()
     results = ocr.predict_image(image)
 
